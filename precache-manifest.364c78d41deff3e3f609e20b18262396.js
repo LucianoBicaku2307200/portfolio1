b5ef2da12cf568522bc413047aaaefe29bc2194b
Luciano Bicaku <luciano.bicaku@gmail.com>
2020-08-25 12:49:49 +0200

    Updates

---
+++ precache-manifest.364c78d41deff3e3f609e20b18262396.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2594e18c23e7a783b9b1b15a17a7261",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "b3fa2e9cf527b228a0fa",
+    "url": "/portfolio1/static/css/main.c22fa10f.chunk.css"
+  },
+  {
+    "revision": "36566283b3d2f9678144",
+    "url": "/portfolio1/static/js/2.3f60267c.chunk.js"
+  },
+  {
+    "revision": "9623c8cb1f44c8b379d19fb3c905dd9f",
+    "url": "/portfolio1/static/js/2.3f60267c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fa2e9cf527b228a0fa",
+    "url": "/portfolio1/static/js/main.b9b685e3.chunk.js"
+  },
+  {
+    "revision": "43cb86accc6e0f3720db",
+    "url": "/portfolio1/static/js/runtime-main.1ca99ad0.js"
+  },
+  {
+    "revision": "c83b83dda7b3ed5dfa25067709ac5dda",
+    "url": "/portfolio1/static/media/img1.c83b83dd.png"
+  },
+  {
+    "revision": "272764788f246ae6bdf122d4eb349c88",
+    "url": "/portfolio1/static/media/img2-md.27276478.jpg"
+  },
+  {
+    "revision": "b55f6423eb559e31f289eb2060e075ca",
+    "url": "/portfolio1/static/media/lock-al.b55f6423.png"
+  },
+  {
+    "revision": "ef2f9b08c38cf597ad90d090f43f7a97",
+    "url": "/portfolio1/static/media/myImg.ef2f9b08.svg"
+  },
+  {
+    "revision": "e144a68493e778422403634906b80c62",
+    "url": "/portfolio1/static/media/projects-2-md.e144a684.jpg"
+  },
+  {
+    "revision": "73933c1681a660153b0a1628d7aaf820",
+    "url": "/portfolio1/static/media/quote-md.73933c16.jpg"
+  },
+  {
+    "revision": "d1e9676f6d92028fd1a4363764b52eb9",
+    "url": "/portfolio1/static/media/skills.d1e9676f.jpg"
+  },
+  {
+    "revision": "12a5da9e683d1b125b170c0da8d2978a",
+    "url": "/portfolio1/static/media/wallpaper.12a5da9e.jpg"
+  }
+]);
